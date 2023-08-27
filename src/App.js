1aeedd9840f6fd29c762939f947f496af2443d80
heeeun730 <heeeunlee3@gmail.com>
2023-08-27 20:52:07 +0900

    메인 페이지만

--- src/App.js
+++ src/App.js
@@ -1,23 +1,292 @@
-import logo from './current.png';
-import './App.css';
-import 'animate.css';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import { MobileView, BrowserView } from 'react-device-detect';
+import CurrentLogo from './current.png';
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  });
   return (
     <div className="App">
-      <img src={logo} className="animate__animated animate__pulse" alt="logo" />
-      <ul>
-        <li>thecurrentkr@gmail.com</li>
-      </ul>
-      <div className="animated-title">
-        <div className="track">
-          <div className="content">
-            we are create for your the
-            current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create for your
-            the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <BrowserView>
+        <div className="animated-title">
+          <div className="track">
+            <div>
+              we are create for your the
+              current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create for your
+              the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are create for
+              your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create
+              for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are
+              create for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we
+              are create for your the
+              current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are create for your
+              the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create for
+              your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are create
+              for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are
+              create for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we
+              are create for your the
+              current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create for your
+              the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are create for
+              your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create
+              for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are
+              create for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we
+              are create for your the
+              current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
           </div>
         </div>
-      </div>
+        <header>
+          <img
+            src={CurrentLogo}
+            className="animate__animated animate__pulse"
+            alt="the current"
+          />
+        </header>
+        <div className="container">
+          <div className="copyright">
+            <p>
+              We are create <br />
+              for your the current
+              <br /> <br />Ⓒ 2023 The Current Lab
+            </p>
+          </div>
+          <div className="content">
+            <section className="sec2">
+              <ul>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="1300"
+                  data-aos-offset="300"
+                  data-aos-once="true"
+                  className="aos-init aos-animate"
+                >
+                  <dl>
+                    <dt>BX</dt>
+                    <dd className="txt0">
+                      Brand Strategy
+                      <br />
+                      Naming
+                      <br />
+                      Application Design
+                      <br />
+                      Signage Design
+                      <br />
+                      Marketing Design
+                    </dd>
+                    <dd className="txt1">
+                      브랜드 전략 컨설팅
+                      <br />
+                      브랜드 네이밍
+                      <br />
+                      온 / 오프라인 어플리케이션
+                      <br />
+                      사이니지 디자인
+                      <br />
+                      SNS 마케팅 디자인
+                    </dd>
+                  </dl>
+                </li>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="1300"
+                  data-aos-offset="300"
+                  data-aos-once="true"
+                  className="aos-init aos-animate"
+                >
+                  <dl>
+                    <dt>UX</dt>
+                    <dd className="txt0">
+                      UX Architecture
+                      <br />
+                      Web Design
+                      <br />
+                      Mobile App Design
+                      <br />
+                      GUI Design
+                      <br />
+                      UX Accelerating
+                    </dd>
+                    <dd className="txt1">
+                      사용자 경험 설계 및 컨설팅
+                      <br />
+                      웹 사이트 디자인
+                      <br />
+                      모바일 앱 디자인
+                      <br />
+                      GUI 디자인
+                      <br />
+                      스타트업 UX 엑셀 레이팅
+                    </dd>
+                  </dl>
+                </li>
+              </ul>
+            </section>
+            <section
+              className="sec3 aos-init aos-animate"
+              data-aos="fade-up"
+              data-aos-duration="1300"
+              data-aos-once="true"
+            >
+              <p>
+                세상의 모든 결과는 고민이 아닌 행동이 만들어냅니다.
+                <br />
+                처음 보는 회사인데 괜찮을까? 비용과 일정은 가능할까? <br />
+                막연한 고민보다 한 번의 연락이 기대 이상의 <br />
+                결과를 만들 수 있습니다.
+              </p>
+            </section>
+            <section
+              className="sec4 aos-init aos-animate"
+              data-aos="fade-up"
+              data-aos-duration="1300"
+              data-aos-once="true"
+            >
+              <p>thecurrentkr@gmail.com</p>
+            </section>
+          </div>
+        </div>
+      </BrowserView>
+      <MobileView>
+        <div className="animated-title">
+          <div className="track">
+            <div>
+              we are create for your the
+              current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create for your
+              the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are create for
+              your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create
+              for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are
+              create for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we
+              are create for your the
+              current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are create for your
+              the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create for
+              your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are create
+              for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are
+              create for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we
+              are create for your the
+              current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create for your
+              the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are create for
+              your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we are create
+              for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; we are
+              create for your the current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we
+              are create for your the
+              current&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+          </div>
+        </div>
+        <header>
+          <img
+            src={CurrentLogo}
+            className="animate__animated animate__pulse"
+            alt="the current"
+          />
+        </header>
+        <div className="container">
+          <div className="copyright">
+            <p>
+              We are create <br />
+              for your the current
+              <br /> <br />Ⓒ 2023 The Current Lab
+            </p>
+          </div>
+          <div className="content">
+            <section className="sec2">
+              <ul>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="1300"
+                  data-aos-offset="300"
+                  data-aos-once="true"
+                  className="aos-init aos-animate"
+                >
+                  <dl>
+                    <dt>BX</dt>
+                    <dd className="txt0">
+                      Brand Strategy
+                      <br />
+                      Naming
+                      <br />
+                      Application Design
+                      <br />
+                      Signage Design
+                      <br />
+                      Marketing Design
+                    </dd>
+                    <dd className="txt1">
+                      브랜드 전략 컨설팅
+                      <br />
+                      브랜드 네이밍
+                      <br />
+                      온 / 오프라인 어플리케이션
+                      <br />
+                      사이니지 디자인
+                      <br />
+                      SNS 마케팅 디자인
+                    </dd>
+                  </dl>
+                </li>
+                <li
+                  data-aos="fade-up"
+                  data-aos-duration="1300"
+                  data-aos-offset="300"
+                  data-aos-once="true"
+                  className="aos-init aos-animate"
+                >
+                  <dl>
+                    <dt>UX</dt>
+                    <dd className="txt0">
+                      UX Architecture
+                      <br />
+                      Web Design
+                      <br />
+                      Mobile App Design
+                      <br />
+                      GUI Design
+                      <br />
+                      UX Accelerating
+                    </dd>
+                    <dd className="txt1">
+                      사용자 경험 설계 및 컨설팅
+                      <br />
+                      웹 사이트 디자인
+                      <br />
+                      모바일 앱 디자인
+                      <br />
+                      GUI 디자인
+                      <br />
+                      스타트업 UX 엑셀 레이팅
+                    </dd>
+                  </dl>
+                </li>
+              </ul>
+            </section>
+            <section
+              className="sec3 aos-init aos-animate"
+              data-aos="fade-up"
+              data-aos-duration="1300"
+              data-aos-once="true"
+            >
+              <p className="mo_txt">
+                세상의 모든 결과는 고민이 아닌 행동이 만들어냅니다.
+                <br />
+                처음 보는 회사인데 괜찮을까? 비용과 일정은 가능할까? <br />
+                막연한 고민보다 한 번의 연락이 기대 이상의 <br />
+                결과를 만들 수 있습니다.
+              </p>
+            </section>
+            <section
+              className="sec4 aos-init aos-animate"
+              data-aos="fade-up"
+              data-aos-duration="1300"
+              data-aos-once="true"
+            >
+              <p className="mo_txt">thecurrentkr@gmail.com</p>
+            </section>
+          </div>
+        </div>
+      </MobileView>
     </div>
   );
 }
