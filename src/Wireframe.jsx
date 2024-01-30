861c1307e195f9e5ed1a03baae3dacb85e3a8657
heeeun730 <heeeunlee3@gmail.com>
2024-01-30 23:13:39 +0900

    swiper 네비게이션 봐주세요

--- src/Wireframe.jsx
+++ src/Wireframe.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
-const Wireframe = (props) => {
-  return <div className="wireframe">s</div>;
-};
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default Wireframe;
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
+
+export default function Wireframe() {
+  return (
+    <div style={{ height: '500px' }}>
+      <Swiper
+        spaceBetween={30}
+        // centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, EffectFade, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="/sample/001.jpeg" alt="img" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/sample/002.jpeg" alt="img" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
